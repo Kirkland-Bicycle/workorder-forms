6bbf048022e2d9b294cbc8af9a6aedec9c3e1550
Parker Couch <parkercouch@users.noreply.github.com>
2017-11-07 10:37:07 -0800

    Create rentalReservation.js

---
+++ rentalReservation.js
@@ -0,0 +1,52 @@
+//Rental Reservation
+
+var today = new Date();
+var currentDate = (today.getMonth() + 1) + "/" + today.getDate();
+var employeeName = document.querySelector("span[data-automation='sidebar-session-employee-name']").textContent;
+var employeeInitial;
+
+switch (employeeName) {
+  case "Parker Couch":
+    employeeInitial = "PC";
+    break;
+  case "Joe Beyler":
+    employeeInitial = "JB";
+    break;
+  case "Nick Miller":
+    employeeInitial = "NM";
+    break;
+  case "Josh Harris":
+    employeeInitial = "JRH";
+    break;
+  case "Matt Caracoglia":
+    employeeInitial = "MC";
+    break;
+  case "Heather Blood":
+    employeeInitial = "HB";
+    break;
+  case "Garrett Stuart":
+    employeeInitial = "GS";
+    break;
+  case "Tom Maurer":
+    employeeInitial = "TM";
+    break;
+  case "James \"Kevin\" Matulis":
+    employeeInitial = "JKM";
+    break;
+  case "Andy Cronin":
+    employeeInitial = "AC";
+    break;
+  default:
+    employeeInitial = "";
+}
+
+var text =
+	"Reserved -- " + employeeInitial + " " + currentDate + ": \n" +
+	"[[ REQUESTS ]] : \n" +
+	"[[ DEPOSIT? ]] : \n" +
+	"[[ OTHER ]] : \n"; 
+
+originalText = document.getElementById("noteTextArea").value;
+document.getElementById("noteTextArea").value = text + originalText;
+document.getElementById("workorder_edit_status_field").value = 16;
+merchantos.edit_workorder.updateStatusFieldTrigger(document.getElementById("workorder_edit_status_field"));
