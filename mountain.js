42f830fa82b71cf3ddfc0d582b3369db3c38ac2a
Parker Couch <parkercouch@users.noreply.github.com>
2017-11-06 17:36:43 -0800

    Update mountain.js

--- mountain.js
+++ mountain.js
@@ -2,7 +2,7 @@
 
 var today = new Date();
 var currentDate = (today.getMonth() + 1) + "/" + today.getDate();
-var employeeName = document.getElementById("session_employee").textContent;
+var employeeName = document.querySelector("span[data-automation='sidebar-session-employee-name']").textContent;
 var employeeInitial;
 
 switch (employeeName) {
@@ -27,23 +27,14 @@ switch (employeeName) {
   case "Garrett Stuart":
     employeeInitial = "GS";
     break;
-  case "Mike Clark":
-    employeeInitial = "MEC";
-    break;
-  case "Devin Sweeney":
-    employeeInitial = "DFS";
-    break;
   case "Tom Maurer":
     employeeInitial = "TM";
     break;
-  case "Zachary Holland":
-  	employeeInitial = "ZH";
-    break;
-  case "Ken Fitzpatrick":
-  	employeeInitial = "KF";
+  case "James \"Kevin\" Matulis":
+    employeeInitial = "JKM";
     break;
   case "Andy Cronin":
-  	employeeInitial = "AC";
+    employeeInitial = "AC";
     break;
   default:
     employeeInitial = "";
