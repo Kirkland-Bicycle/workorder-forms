992da5404f1bb70f056ecacbe9777650751df692
Parker Couch <parkercouch@users.noreply.github.com>
2019-04-18 11:43:45 -0700

    Remove Employee Initials

--- appointment.js
+++ appointment.js
@@ -2,56 +2,17 @@
 
 var today = new Date();
 var currentDate = (today.getMonth() + 1) + "/" + today.getDate();
-var employeeName = document.querySelector("span[data-automation='sidebar-session-employee-name']").textContent;
-var employeeInitial;
 
-switch (employeeName) {
-  case "Parker Couch":
-    employeeInitial = "PC";
-    break;
-  case "Joe Beyler":
-    employeeInitial = "JB";
-    break;
-  case "Nick Miller":
-    employeeInitial = "NM";
-    break;
-  case "Josh Harris":
-    employeeInitial = "JRH";
-    break;
-  case "Matt Caracoglia":
-    employeeInitial = "MC";
-    break;
-  case "Nicholas Rotter":
-    employeeInitial = "NR";
-    break;
-  case "Chase Stockwell":
-    employeeInitial = "CS";
-    break;
-  case "Garrett Stuart":
-    employeeInitial = "GS";
-    break;
-  case "Tom Maurer":
-    employeeInitial = "TM";
-    break;
-  case "James \"Kevin\" Matulis":
-    employeeInitial = "JKM";
-    break;
-  case "Andy Cronin":
-    employeeInitial = "AC";
-    break;
-  default:
-    employeeInitial = "";
-}
 
 var text =
   "** DO FULL CHECK-IN PROCESS WHEN BIKE IS DROPPED OFF **\n" +
-  "Appointment Booked -- " + employeeInitial + " " + currentDate + ": \n" +
+  "Appointment Booked -- " + currentDate + ": \n" +
 	"[[ REQUESTS ]] : \n" +
 	"[[ DEPOSIT? ]] : \n" +
   "[[ ESTIMATE QUOTED ]] : \n" +
 	"[[ OTHER ]] : \n"; 
 
-hookInText = "APPT - " + employeeInitial;
+hookInText = "APPT";
 document.getElementById("hookInInputField").value += hookInText;
 
 originalText = document.getElementById("noteTextArea").value;
